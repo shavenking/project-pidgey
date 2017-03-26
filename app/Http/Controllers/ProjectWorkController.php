fbc5c888bbe5475123eb1fecaf308d8f01a4b20a
shavenking <shavenking@gmail.com>
2017-03-26 12:19:28 +0800

    Add v1 API for creating ProjectWork

--- app/Http/Controllers/ProjectWorkController.php
+++ app/Http/Controllers/ProjectWorkController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Project;
+use App\{
+    Project,
+    Work
+};
 use JWTAuth;
 use Illuminate\Http\Request;
 
@@ -29,5 +32,42 @@ class ProjectWorkController extends Controller
         });
 
         return response()->json(['data' => $works]);
+    }
+
+    public function create(Request $request, Project $project)
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+
+        // currently only project owner can access
+        if ($project->user_id !== $user->id) {
+            return response()->json([], 403);
+        }
+
+        if (!$request->has('work_id') && !$request->has('name')) {
+            return response()->json([], 400);
+        }
+
+        if ($request->has('work_id')) {
+            $stdWork = Work::with('workItems')->find($request->work_id);
+        }
+
+        $work = $project->works()->create([
+            'name' => isset($stdWork) ? $stdWork->name : $request->name,
+            'amount' => $request->amount,
+            'unit_price' => isset($stdWork) ? $stdWork->unit_price : '0.00',
+            'engineering_type_id' => isset($stdWork) ? $stdWork->engineering_type_id : $request->engineering_type_id
+        ]);
+
+        $data = array_merge(
+            array_only($work->toArray(), ['id', 'name', 'amount', 'unit_price', 'engineering_type_id', 'project_id']),
+            [
+                'engineering_type_main_title' => $work->engineeringType->main_title,
+                'engineering_type_detailing_title' => $work->engineeringType->detailing_title
+            ]
+        );
+
+        // TODO: 標準工料項目也得自動加入
+
+        return response()->json(compact('data'), 201);
     }
 }
--- tests/Feature/ProjectWorkTest.php
+++ tests/Feature/ProjectWorkTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature;
 
 use App\{
+    User,
     Project,
-    ProjectWork
+    ProjectWork,
+    Work
 };
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -45,5 +47,82 @@ class ProjectWorkTest extends TestCase
 
         $this->jsonWithToken('GET', "/api/v1/projects/{$project->id}/works")
             ->assertStatus(403);
+    }
+
+    /**
+     * 使用者可以選擇標準工項新增到專案
+     */
+    public function testCreateWithWorkId()
+    {
+        $stdWork = factory(Work::class)->create();
+        $this->user = factory(User::class)->create();
+        $project = factory(Project::class)->create(['user_id' => $this->user->id]);
+
+        $response = $this->jsonWithToken('POST', "/api/v1/projects/{$project->id}/works", [
+            'work_id' => $stdWork->id,
+            'amount' => '10.55'
+        ]);
+
+        $attrs = [
+            'name' => $stdWork->name,
+            'amount' => '10.55',
+            'unit_price' => $stdWork->unit_price,
+            'engineering_type_id' => $stdWork->engineering_type_id
+        ];
+
+        $this->assertDatabaseHas($project->works()->getRelated()->getTable(), $attrs);
+
+        $work = $project->works()->where($attrs)->with('engineeringType')->first();
+
+        $data = array_merge(
+            array_only($work->toArray(), ['id', 'name', 'amount', 'unit_price', 'engineering_type_id', 'project_id']),
+            [
+                'engineering_type_main_title' => $work->engineeringType->main_title,
+                'engineering_type_detailing_title' => $work->engineeringType->detailing_title
+            ]
+        );
+
+        $response->assertStatus(201)->assertExactJson(compact('data'));
+
+        // TODO: 驗證標準工料項目也要加入
+    }
+
+    /**
+     * 使用者可以建立全新專案工項
+     */
+    public function testCreateNew()
+    {
+        $this->user = factory(User::class)->create();
+        $project = factory(Project::class)->create(['user_id' => $this->user->id]);
+        $work = factory(ProjectWork::class)->make();
+
+        $response = $this->jsonWithToken('POST', "/api/v1/projects/{$project->id}/works", [
+            'name' => $work->name,
+            'amount' => $work->amount,
+            'engineering_type_id' => $work->engineering_type_id
+        ]);
+
+        $attrs = [
+            'name' => $work->name,
+            'amount' => $work->amount,
+            'unit_price' => '0.00',
+            'engineering_type_id' => $work->engineering_type_id
+        ];
+
+        $this->assertDatabaseHas($project->works()->getRelated()->getTable(), $attrs);
+
+        $work = $project->works()->where($attrs)->with('engineeringType')->first();
+
+        $data = array_merge(
+            array_only($work->toArray(), ['id', 'name', 'amount', 'unit_price', 'engineering_type_id', 'project_id']),
+            [
+                'engineering_type_main_title' => $work->engineeringType->main_title,
+                'engineering_type_detailing_title' => $work->engineeringType->detailing_title
+            ]
+        );
+
+        $response->assertStatus(201)->assertExactJson(compact('data'));
+
+        // 需檢查全新的專案工項沒有任何專案工料
     }
 }
